aa47cd8c57598108dc93b1a87b1c20a00359bad9
Fred B <fredbitenyo@gmail.com>
2023-05-09 11:01:06 +0200

    correcting s3 main file

--- s3/main.tf
+++ s3/main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "env_file_bucket" {
 
 # upload the environment file from local computer into the s3 bucket
 resource "aws_s3_object" "upload_env_file" {
-  bucket = var.env_bucket_name
+  bucket = "${var.project_name}-${var.env_bucket_name}"
   key    = var.env_filename
   source = "./${var.env_filename}"
 }
